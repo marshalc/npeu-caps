c9a3847641aa2dc4cfe82cb2329b042c1fd11553
Carl Marshall <carl@cm13.net>
2012-07-10 16:49:46 +0100

    Basic CSV Export demo Added link to demo site

--- caps/views.py
+++ caps/views.py
@@ -5,10 +5,19 @@ from caps.models import CapsForm
 
 # Create your views here.
 def home(request):
+    """
+    Default homepage
+    """
     return render_to_response('index.html', {}, context_instance=RequestContext(request))
 
 @staff_member_required
 def admin_view_all_csv(request):
+    """
+    Display a listing of all CAPS reports as a CSV file, but restrict to staff members
+    """
     listing = CapsForm.objects.all().select_related()
-    return render_to_response('admin/list-caps.csv', {'listing':listing},
-        context_instance=RequestContext(request))
+    return render_to_response(
+        'admin/list-caps.csv',
+        { 'listing':listing },
+        context_instance=RequestContext(request)
+    )
--- npeu/urls.py
+++ npeu/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-import caps
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-#    url(r'^admin/caps/capsform/view/all/', caps.views.admin_view_all),
+    url(r'^admin/caps/capsform/view/all/', 'caps.views.admin_view_all_csv'),
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', 'caps.views.home', name='home'),
